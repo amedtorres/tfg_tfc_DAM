b00e546f0b27f82e3f7ce1dcdb9cb2ef82dcf72e
Amed Torres <amedtorres15@gmail.com>
2026-04-05 20:44:26 +0200

    Actualizar layouts y renombrar fragmentos

--- app/src/main/java/com/amedtorres/bagdrop/ui/fragmentReservar/ReservarFragment.kt
+++ app/src/main/java/com/amedtorres/bagdrop/ui/fragmentReservar/ReservarFragment.kt
@@ -1,4 +1,4 @@
-package com.amedtorres.bagdrop.ui.fragment
+package com.amedtorres.bagdrop.ui.fragmentReservar
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ReservaFragment.newInstance] factory method to
+ * Use the [ReservarFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ReservaFragment : Fragment() {
+class ReservarFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class ReservaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reserva, container, false)
+        return inflater.inflate(R.layout.fragment_reservar, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class ReservaFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ReservaFragment.
+         * @return A new instance of fragment ReservarFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ReservaFragment().apply {
+            ReservarFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
